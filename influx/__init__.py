c17a2b1df7e57a0236056b66f22d1cd51b629902
Jure Ham <jure@hamsworld.net>
2016-03-08 12:41:26 +0100

    First

---
+++ influx/__init__.py
@@ -0,0 +1,73 @@
+import functools
+import time
+
+from contextlib import contextmanager
+
+import statsd
+from django.conf import settings
+
+
+_telegraf_client = None
+
+
+def _get_client():
+    global _telegraf_client
+
+    if _telegraf_client is None:
+        _telegraf_client = statsd.StatsClient(settings.STATSD_INFLUX_HOST, settings.STATSD_INFLUX_PORT)
+
+    return _telegraf_client
+
+
+def _get_source():
+    return settings.HOSTNAME.replace('.', '-')
+
+
+def _get_default_tags():
+    return [('host', _get_source())]
+
+
+def _get_tags(custom_tags):
+    tags = sorted(custom_tags.items(), key=lambda x: x[0]) + _get_default_tags()
+
+    return ','.join('{0}={1}'.format(k, v) for k, v in tags)
+
+
+@contextmanager
+def block_timer(name, **tags):
+    start = time.time()
+    yield
+    new_name = '{prefix}.{name},{tags}'.format(
+        prefix=settings.PROJECT_NAME,
+        source=_get_source(),
+        name=name,
+        tags=_get_tags(tags),
+    )
+    _get_client().timing(new_name, int((time.time() - start) * 1000))
+
+
+def timer(name, **tags):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            with block_timer(name, **tags):
+                result = func(*args, **kwargs)
+            return result
+        return wrapper
+    return decorator
+
+
+def incr(name, count, **tags):
+    _get_client().incr('{prefix}.{name},{tags}'.format(
+        prefix=settings.PROJECT_NAME,
+        name=name,
+        tags=_get_tags(tags),
+    ), count)
+
+
+def gauge(name, value, **tags):
+    _get_client().gauge('{prefix}.{name},{tags}'.format(
+        prefix=settings.PROJECT_NAME,
+        name=name,
+        tags=_get_tags(tags),
+    ), value)
---
+++ setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+import sys
+
+
+class Tox(TestCommand):
+    user_options = [('tox-args=', 'a', "Arguments to pass to tox")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.tox_args = None
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import tox
+        import shlex
+        args = self.tox_args
+        if args:
+            args = shlex.split(self.tox_args)
+        errno = tox.cmdline(args=args)
+        sys.exit(errno)
+
+
+setup(
+    name='django-statsd-influx',
+    packages=['influx'],
+    version='0.1',
+    description='Django Statsd Influx Client ',
+    author='Jure Ham, Zemanta',
+    author_email='jure.ham@zemanta.com',
+    url='https://github.com/Zemanta/django-statsd-influx',
+    download_url='https://github.com/Zemanta/django-statsd-influx/tarball/0.1',
+    keywords=['statsd', 'influx', 'influxdb', 'django'],
+    install_requires=[
+        'statsd==3.2.1',
+        'Django>=1.8.4'
+    ],
+    tests_require=['tox', 'virtualenv', 'statsd==3.2.1', 'Django>=1.8.4'],
+    cmdclass={'test': Tox},
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
